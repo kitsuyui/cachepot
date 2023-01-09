71622de3c68ea72513dcc0b6c6590d26b031ef08
kitsuyui <kitsuyui@kitsuyui.com>
2023-01-09 21:39:59 +0900

    Rewrite tests for pytest

--- tests/backend/test_filesystem.py
+++ tests/backend/test_filesystem.py
@@ -1,34 +1,32 @@
 import pathlib
 import tempfile
 import time
-import unittest
 
 from cachepot.backend.filesystem import FileSystemCacheBackend
 
 
-class TestFileSystemCacheBackend(unittest.TestCase):
+def test_various_pathlike() -> None:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        cachestore = FileSystemCacheBackend(pathlib.Path(tmpdir))
+        assert cachestore.load(b"1") is None
+        cachestore.save(b"1", b"2", expire_seconds=1)
+        assert cachestore.load(b"1") == b"2"
+        cachestore.delete(b"1")
+        assert cachestore.load(b"1") is None
 
-    def test_various_pathlike(self) -> None:
-        with tempfile.TemporaryDirectory() as tmpdir:
-            cachestore = FileSystemCacheBackend(pathlib.Path(tmpdir))
-            self.assertEqual(cachestore.load(b'1'), None)
-            cachestore.save(b'1', b'2', expire_seconds=1)
-            self.assertEqual(cachestore.load(b'1'), b'2')
-            cachestore.delete(b'1')
-            self.assertEqual(cachestore.load(b'1'), None)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        cachestore = FileSystemCacheBackend(tmpdir)
+        assert cachestore.load(b"3") is None
+        cachestore.save(b"3", b"4", expire_seconds=1)
+        assert cachestore.load(b"3") == b"4"
+        cachestore.delete(b"3")
+        assert cachestore.load(b"3") is None
 
-        with tempfile.TemporaryDirectory() as tmpdir:
-            cachestore = FileSystemCacheBackend(tmpdir)
-            self.assertEqual(cachestore.load(b'3'), None)
-            cachestore.save(b'3', b'4', expire_seconds=1)
-            self.assertEqual(cachestore.load(b'3'), b'4')
-            cachestore.delete(b'3')
-            self.assertEqual(cachestore.load(b'3'), None)
 
-    def test_expire(self) -> None:
-        with tempfile.TemporaryDirectory() as tmpdir:
-            cachestore = FileSystemCacheBackend(pathlib.Path(tmpdir))
-            cachestore.save(b'1', b'2', expire_seconds=1)
-            self.assertEqual(cachestore.load(b'1'), b'2')
-            time.sleep(2)
-            self.assertEqual(cachestore.load(b'1'), None)
+def test_expire() -> None:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        cachestore = FileSystemCacheBackend(pathlib.Path(tmpdir))
+        cachestore.save(b"1", b"2", expire_seconds=1)
+        assert cachestore.load(b"1") == b"2"
+        time.sleep(2)
+        assert cachestore.load(b"1") is None
--- tests/test_expire.py
+++ tests/test_expire.py
@@ -1,17 +1,15 @@
-import unittest
 from datetime import timedelta
 
 from cachepot.expire import to_timedelta
 
 
-class TestExpireSeconds(unittest.TestCase):
+def test_to_timedelta_float() -> None:
+    assert to_timedelta(1.0) == timedelta(seconds=1.0)
 
-    def test_to_timedelta(self) -> None:
-        # float
-        self.assertEqual(to_timedelta(1.0), timedelta(seconds=1.0))
 
-        # int
-        self.assertEqual(to_timedelta(3), timedelta(seconds=3))
+def test_to_timedelta_int() -> None:
+    assert to_timedelta(3) == timedelta(seconds=3)
 
-        # timedelta
-        self.assertEqual(timedelta(days=5), timedelta(days=5))
+
+def test_to_timedelta_timedelta() -> None:
+    assert to_timedelta(timedelta(days=5)) == timedelta(days=5)
